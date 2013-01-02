3765e52edbe322156ef0117b1f63937e7c824834
Jonne Nauha <jonne.nauha@adminotech.com>
2013-01-02 03:04:56 +0200

    Fix detecting OS also for Qt 4.7, Q_OS_WIN does not exist there.

--- src/qhttpserverapi.h
+++ src/qhttpserverapi.h
@@ -25,6 +25,7 @@
 
 #include <QtGlobal>
 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
 #ifdef Q_OS_WIN
     // Define to export or import depending if we are building or using the library.
     // QHTTPSERVER_EXPORT should only be defined when building.
@@ -36,6 +37,20 @@
 #else
     // Define empty for other platforms
     #define QHTTPSERVER_API
+#endif
+#else
+#ifdef Q_WS_WIN
+    // Define to export or import depending if we are building or using the library.
+    // QHTTPSERVER_EXPORT should only be defined when building.
+    #if defined(QHTTPSERVER_EXPORT)
+        #define QHTTPSERVER_API __declspec(dllexport)
+    #else
+        #define QHTTPSERVER_API __declspec(dllimport)
+    #endif
+#else
+    // Define empty for other platforms
+    #define QHTTPSERVER_API
+#endif
 #endif
 
 #endif
